539f1eee7a455558f7da1267904f8f2b07be554f
Alex Barker <alexbark@gmail.com>
2019-08-09 09:40:52 +1000

    Add agent information to component

--- src/app/components/agent.component.ts
+++ src/app/components/agent.component.ts
@@ -4,8 +4,32 @@ import { Agent } from './agent.model';
 
 @Component({
   selector: 'app-agent',
+  styleUrls: ['./agent.component.scss'],
   template: `
-    <h3>{{agent.Name}} says:</h3>
+    <div class="agent-container">
+      <div class="agent-icon">
+        <img src="assets/avatar.png"/>
+      </div>
+      <div class="agent-info">
+        <p>{{agent.Name}}</p>
+        <p>{{agent.Agency.Name}}</p>
+      </div>
+      <div class="agent-reviews">
+        <img src="assets/chat.png"/>
+        <p>{{agent.TotalReviews}}</p>
+        <p>Reviews</p>
+      </div>
+      <div class="agent-sold-properties">
+        <img src="assets/gavel.png"/>
+        <p>{{agent.SoldProperties}}</p>
+        <p>Sold properties in <b>Melbourne</b></p>
+      </div>
+      <div class="agent-average-sale-price">
+        <img src="assets/money.png"/>
+        <p>{{agent.AverageSalePrice}}</p>
+        <p>Average sale price in <b>Melbourne</b></p>
+      </div>
+    </div>
   `
 })
 export class AgentComponent {
